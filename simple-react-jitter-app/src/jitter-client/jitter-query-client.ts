c4fd532349b2e558dc145e14bae2195747bb246b
Erik <erik@Eriks-MacBook-Air.local>
2026-06-23 14:42:22 +0200

    changed number of retries

--- simple-react-jitter-app/src/jitter-client/jitter-query-client.ts
+++ simple-react-jitter-app/src/jitter-client/jitter-query-client.ts
@@ -4,7 +4,7 @@ import calculateRetryDelay from './calculate-retry-delay.ts'
 export const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
-            retry: 5,
+            retry: 3,
             retryDelay: (retryCount: number) => {
                 return calculateRetryDelay(retryCount, {
                     baseMultiplier: 2,
